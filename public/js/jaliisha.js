565c84227bed25303b64359daf0d411eb5af598f
Emily Lee <eseitz@alum.mit.edu>
2017-04-24 23:26:44 -0700

    logging info

--- public/js/jaliisha.js
+++ public/js/jaliisha.js
@@ -36,31 +36,37 @@ function checkForYes () {
 }
 
 $('#find-name-btn').click(function () {
+    var first = $('#first').val(),
+        last = $('#last').val();
+    console.log('RSVP CALL    [find name] ' + last + ', ' + first);
+    
     $('#alerts .alert:visible').hide();
     var that = this;
-    console.log('find name');
     $.ajax({
         url: '/invite',
         data: {
-            first: $('#first').val(),
-            last: $('#last').val()
+            first: first,
+            last: last
         },
         success: function (data, success, jqxhr) {
             if (data.error) {
-                console.log('ERROR', data.error);
+                console.log('RSVP ERROR   [find name] ' + last + ', ' + first + ' - ' + data.error);
             } else if (data.guests !== null) {
+                console.log('RSVP SUCCESS [find name] ' + last + ', ' + first + ' - family: ' + data.guests[0].family);
+                
                 // found the guest
                 $(that).hide();
                 $('#find-name input').attr('disabled', true);
                 $('#find-name').hide();
                 showRsvpOptions(data.guests, data.rsvpList);
             } else {
+                console.log('RSVP WARNING [find name] ' + last + ', ' + first + ' - not found');
                 $('#alerts #guest-no').show();
                 $('#find-name input').val('');
             }
         },
         error: function (jqxhr, status, errorMsg) {
-            console.log('ERROR', status, errorMsg);
+            console.log('RSVP ERROR   [find name] ' + last + ', ' + first + ' - ' + status + ': ' + errorMsg);
         }
     });
 });
@@ -87,29 +93,37 @@ $('#edit-rsvp-btn').click(function () {
 
 $('#submit-btn').click(function () {
     $('#alerts .alert:visible').hide();
-    var that = this;
-    console.log('submit, done');
+    
     var rsvps = [];
     $('.rsvp-guest').each(function (i, el) {
+        var first = $(this).data('first'),
+            last = $(this).data('last'),
+            rsvp = $(this).find('input:radio:checked').val();
+        console.log('RSVP CALL    [rsvp done] ' + last + ', ' + first + ' - ' + rsvp);
         rsvps.push({
-            first: $(this).data('first'),
-            last: $(this).data('last'),
-            rsvp: $(this).find('input:radio:checked').val(),
+            first: first,
+            last: last,
+            rsvp: rsvp,
         });
     });
     
+    var diet = $('#edit-rsvp #diet').val(),
+        note = $('#edit-rsvp #note').val()
+    console.log('RSVP CALL    [rsvp done] ' + rsvps[0].last + ', ' + rsvps[0].first + ' - diet: ' + diet);
+    console.log('RSVP CALL    [rsvp done] ' + rsvps[0].last + ', ' + rsvps[0].first + ' - note: ' + note);
+    
     $.post('/submit', {
             rsvps: rsvps,
-            diet: $('#comments #diet').val(),
-            note: $('#comments #note').val()
+            diet: diet,
+            note: note
         },
         function (data, success, jqxhr) {
             if (data.error) {
-                console.log('ERROR', data.error);
+                console.log('RSVP ERROR   [rsvp done] ' + rsvps[0].last + ', ' + rsvps[0].first + ' - ' + data.error);
             } else {
+                console.log('RSVP SUCCESS [rsvp done] ' + rsvps[0].last + ', ' + rsvps[0].first);
                 $('#find-name').hide();
                 $('#edit-rsvp').hide();
-                $('#comments').hide();
                 $('#btn-container').hide();
                 if (data.rsvp) {
                     $('#alerts #success-yes').show();
