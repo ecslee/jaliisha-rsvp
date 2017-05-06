11071def243f627d796d1fa068a271803d942b4e
Emily Lee <eseitz@alum.mit.edu>
2017-05-06 15:39:29 -0700

    small style edits and cleanup

--- routes/admin.js
+++ routes/admin.js
@@ -20,44 +20,6 @@ var doc = new AWS.DynamoDB.DocumentClient({
     }
 });
 
-function renderRSVP(res) {
-    var response = {
-        yes: [],
-        no: [],
-        na: []
-    };
-    
-    doc.scan({
-        ProjectionExpression: "#l, #f, #d, #fam, #rsvp, #diet, #song, #note",
-        ExpressionAttributeNames: {
-            "#l": "last",
-            "#f": "first",
-            "#d": "display",
-            "#fam": "fam",
-            "#rsvp": "rsvp",
-            "#diet": "diet",
-            "#song": "song",
-            "#note": "note"
-        },
-    }, function (err, data) {
-        if (err) {
-            response.error = err;
-        }
-        
-        data.Items.forEach(function (item, i) {
-            if (typeof item.rsvp === 'boolean' && item.rsvp === true) {
-                response.yes.push(item);
-            } else if (typeof item.rsvp === 'boolean' && item.rsvp === false) {
-                response.no.push(item);
-            } else {
-                response.na.push(item);
-            }
-        });
-        
-        res.render('admin', response);
-    });
-}
-
 router.get('/', function (req, res) {
     res.render('admin-password');
 });
@@ -99,18 +61,6 @@ router.get('/rsvps', function (req, res) {
         
         res.render('admin', response);
     });
-    
-//    EJS.renderFile(
-//        'views/admin.ejs',
-//        {},
-//        function (err, str) {
-//            if (err) {
-//                response.error = err;
-//            }
-//            response.rsvpChart = str;
-//            res.end(JSON.stringify(response));
-//        }
-//    );
 });
 
 router.post('/auth', function (req, res) {
