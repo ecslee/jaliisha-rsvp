1c55096902acf6f782d1668e613325e2964d4ed9
Emily Lee <eseitz@alum.mit.edu>
2017-05-06 15:08:01 -0700

    password landing page for admin console

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,43 @@
+$('#submit-btn').click(function () {
+    var password = $('#password').val().trim();
+
+    if (password) {
+        $('#alerts .alert').hide();
+        
+        $.post('/admin/auth', {
+                password: password
+            },
+            function (data, success, jqxhr) {
+                if (data.error) {
+                    $('#alerts #error').show();
+                } else {
+                    if (!data.auth) {
+                        $('#alerts #auth-no').show();
+                    } else {
+                        $.ajax({
+                            url: '/admin/rsvps',
+                            success: function (data, success, jqxhr) {
+                                if (data.error) {
+                                    $('#alerts #error').show();
+                                } else {
+                                    console.log('success');
+                                    $('#find-name').hide();
+                                    $('#btn-container').hide();
+                                    $('#header').hide();
+                                    $('#rsvp-table').html(data).show();
+                                }
+                            },
+                            error: function (jqxhr, status, errorMsg) {
+                                $('#alerts #error').show();
+                            }
+                        });
+                    }
+                }
+            }
+        );
+    }
+});
+
 $('[data-toggle="popover"]').popover({
     placement: 'top',
     trigger: 'focus',
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var EJS = require('ejs');
+
 var AWS = require('aws-sdk');
 
 var db = new AWS.DynamoDB({
@@ -18,7 +20,7 @@ var doc = new AWS.DynamoDB.DocumentClient({
     }
 });
 
-router.get('/', function (req, res) {
+function renderRSVP(res) {
     var response = {
         yes: [],
         no: [],
@@ -38,9 +40,6 @@ router.get('/', function (req, res) {
             "#note": "note"
         },
     }, function (err, data) {
-        console.log('err', err);
-        console.log('data', data);
-        
         if (err) {
             response.error = err;
         }
@@ -57,6 +56,89 @@ router.get('/', function (req, res) {
         
         res.render('admin', response);
     });
+}
+
+router.get('/', function (req, res) {
+    res.render('admin-password');
+});
+
+router.get('/rsvps', function (req, res) {
+    var response = {
+        yes: [],
+        no: [],
+        na: [],
+        error: null
+    };
+    
+    doc.scan({
+        ProjectionExpression: "#l, #f, #d, #fam, #rsvp, #diet, #song, #note",
+        ExpressionAttributeNames: {
+            "#l": "last",
+            "#f": "first",
+            "#d": "display",
+            "#fam": "fam",
+            "#rsvp": "rsvp",
+            "#diet": "diet",
+            "#song": "song",
+            "#note": "note"
+        },
+    }, function (err, data) {
+        if (err) {
+            response.error = err;
+        }
+        
+        data.Items.forEach(function (item, i) {
+            if (typeof item.rsvp === 'boolean' && item.rsvp === true) {
+                response.yes.push(item);
+            } else if (typeof item.rsvp === 'boolean' && item.rsvp === false) {
+                response.no.push(item);
+            } else {
+                response.na.push(item);
+            }
+        });
+        
+        res.render('admin', response);
+    });
+    
+//    EJS.renderFile(
+//        'views/admin.ejs',
+//        {},
+//        function (err, str) {
+//            if (err) {
+//                response.error = err;
+//            }
+//            response.rsvpChart = str;
+//            res.end(JSON.stringify(response));
+//        }
+//    );
+});
+
+router.post('/auth', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var password = req.body.password;
+    
+    db.getItem({
+        Key: {
+            first: { S: 'ALEKSI' },
+            last: { S: 'LEE' }
+        }
+    }, function (err, data) {
+        var response = {
+            error: null,
+            auth: false
+        }
+        
+        if (err) {
+            response.error = err
+        }
+        
+        if (data.Item && data.Item.fam && data.Item.fam.S === password) {
+            response.auth = true;
+        }
+        
+        res.end(JSON.stringify(response));
+    });
+    
 });
 
 module.exports = router;
